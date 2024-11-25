2d9cd44e768e3e6e8d55921abcc4e3a6353e8bdc
ajeetram <86039147+ajeetram@users.noreply.github.com>
2024-11-25 13:21:21 +0530

    desc added

--- src/Components/WalletInfo/WalletInfo.jsx
+++ src/Components/WalletInfo/WalletInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState,useContext} from 'react';
 import {ethers } from 'ethers';
-import { MdLogout, MdOutlineContentCopy } from 'react-icons/md';
+import { MdLogout, MdOutlineContentCopy,MdClose } from 'react-icons/md';
 import { FaRegCircle } from "react-icons/fa6";
 import './wallet.css'; // Import the CSS file
 import { EdubukContexts } from '../../Context/EdubukContext';
@@ -8,13 +8,14 @@ import toast from 'react-hot-toast';
 import SmallLoader from '../SmallLoader/SmallLoader';
 
 const WalletInfo = ({showWalletInfo,setShowWalletInfo}) => {
-  const { account,setAccount,chainId} = useContext(EdubukContexts);
+  const { account,setAccount} = useContext(EdubukContexts);
+  const [chainId,setChainId]=useState(null);
   const [balance,setBalance] = useState(null);
-
   const tokenName = {
     1:"ETH",
     51:"TXDC",
     50:"XDC",
+    97:"TBNB",
     56:"BNB",
     974399131:"SFUEL",
   }
@@ -25,7 +26,10 @@ const WalletInfo = ({showWalletInfo,setShowWalletInfo}) => {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const balance = await provider.getBalance(account);
       const etherBalance = ethers.utils.formatEther(balance); 
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const currChainId = await window.ethereum.request({ method: 'eth_chainId' });
       const roundedBalance = parseFloat(etherBalance).toFixed(6)
+      setChainId(parseInt(currChainId, 16));
       setBalance(roundedBalance);
     } catch (error) {
       console.error('Error fetching balance:', error);
@@ -56,7 +60,6 @@ const WalletInfo = ({showWalletInfo,setShowWalletInfo}) => {
     }
   }
 
-  
 
   useEffect(() => {
     getAccountBalance();
@@ -68,7 +71,10 @@ const WalletInfo = ({showWalletInfo,setShowWalletInfo}) => {
       <div className={`wallet-container ${showWalletInfo ? 'active' : ''}`}>
         <div className="wallet-box">
           <div className='wallet-acc-info'>
+          <div className='wallet-header'>
             <h3><FaRegCircle id='circle' /> <span>{account?.substring(0, 6)}...{account?.substring(account.length - 5)}</span></h3>
+            <MdClose onClick={()=>setShowWalletInfo(false)}/>
+            </div>
             {balance?<p>
               <strong>Balance: <span>{balance} {tokenName[chainId]}</span></strong>
             </p>:<div id="balance-loader"><SmallLoader /></div>}
